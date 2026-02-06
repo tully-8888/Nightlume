fd41c238a4e5213ba79192801b2778ae1a4b3c60
tully <60903950+tully-8888@users.noreply.github.com>
2026-02-06 18:07:40 +0200

    Fix DMG script qmake6 release generation

--- scripts/generate-dmg.sh
+++ scripts/generate-dmg.sh
@@ -40,7 +40,7 @@ mkdir $INSTALLER_FOLDER
 
 echo Configuring the project
 pushd $BUILD_FOLDER
-qmake $SOURCE_ROOT/moonlight-qt.pro QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" || fail "Qmake failed!"
+qmake6 $SOURCE_ROOT/moonlight-qt.pro CONFIG+=release QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" || fail "Qmake failed!"
 popd
 
 echo Compiling Moonlight in $BUILD_CONFIG configuration
